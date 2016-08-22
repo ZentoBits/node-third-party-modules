e26659807eed35bc1707ebc161bf8c542cc8b4c5
Zachary George <zacharygeorge@Zacharys-MacBook-Pro.local>
2016-08-22 09:22:51 -0500

    made a flag in console

---
+++ 06.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+var chalk = require('chalk');
+
+console.log(chalk.white.bgBlue('  *  *  *  *  *  *  *  ') + chalk.bgRed('                                                    '));
+console.log(chalk.white.bgBlue('   *  *  *  *  *  *    ') + chalk.bgWhite('                                                    '))
+console.log(chalk.white.bgBlue('  *  *  *  *  *  *  *  ') + chalk.bgRed('                                                    '));
+console.log(chalk.white.bgBlue('   *  *  *  *  *  *    ') + chalk.bgWhite('                                                    '))
+console.log(chalk.white.bgBlue('  *  *  *  *  *  *  *  ') + chalk.bgRed('                                                    '));
+console.log(chalk.white.bgBlue('   *  *  *  *  *  *    ') + chalk.bgWhite('                                                    '))
+console.log(chalk.white.bgBlue('  *  *  *  *  *  *  *  ') + chalk.bgRed('                                                    '));
+console.log(chalk.bgWhite('                                                                           '))
+console.log(chalk.bgRed('                                                                           '))
+console.log(chalk.bgWhite('                                                                           '))
+console.log(chalk.bgRed('                                                                           '))
+console.log(chalk.bgWhite('                                                                           '))
+console.log(chalk.bgRed('                                                                           '))
